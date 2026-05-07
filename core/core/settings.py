99891c36494bc0237b21a530bdff5a579b7c9c47
Diego Buencuero <Diegobuencuero4095@gmail.com>
2026-05-07 11:04:37 -0300

    traducciones

--- core/core/settings.py
+++ core/core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'pt'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
@@ -123,10 +125,8 @@ USE_TZ = True
 from django.utils.translation import gettext_lazy as _
 
 LANGUAGES = (
-  
+    ('pt', _('Português')),
     ('es', _('Español')),
-    ('pt', _('Portugues')),
-  
 )
 
 LOCALE_PATHS = (
